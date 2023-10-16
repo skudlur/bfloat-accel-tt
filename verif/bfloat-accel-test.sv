402c508acd3adf59245541a537814530aac602d9
skudlur <suhaskv0111@gmail.com>
2023-10-16 22:41:22 +0530

    Fix: coverage

--- verif/bfloat-accel-test.sv
+++ verif/bfloat-accel-test.sv
@@ -5,7 +5,16 @@
 
 
 // Program block 
-program test_bfloat;
+// Code your testbench here
+// or browse Examples
+// Testbench for bfloat-adder.sv
+// ===================================
+// Inputs: [15:0] a, [15:0] b,
+// Outputs: [15:0] out
+
+
+// Program block 
+module test_bfloat;
 
    // Parameters
    parameter NUM_TESTS = 100;
@@ -18,57 +27,60 @@ program test_bfloat;
 
    // Coverage
    covergroup bf16adder_coverage;
+      option.per_instance = 1;
+     
       // Coverpoints for 16-bit a and b inputs
       coverpoint input1 {
-	 bins in_bin = { [0:65535] };
+        bins neg_in_bin[] = { [-31766:-1] };
+        bins zero_in_bin = { 0 };
+        bins pos_in_bin[] = { [1:32767] };
       }
       coverpoint input2 {
-	 bins in_bin = { [0:65535] };
+        bins neg_in_bin[] = { [-31766:-1] };
+        bins zero_in_bin = { 0 };
+        bins pos_in_bin[] = { [1:32767] };
       }
 
       // Coverpoints for 16-bit output
       coverpoint out {
-	 bins out_bin = { [0:65535] };
+        bins neg_out_bin[] = { [-31766:-1] };
+        bins zero_out_bin = { 0 };
+        bins pos_out_bin[] = { [1:32767] };
       }
 
       // Cross coverage for inputs
-      cross input_cross = {
-	 input1,
-	 input2,
-	 out
-      };
+      cross input1, input2;
    endgroup // bf16adder_coverage
 
    // covergroup instance
-   bf16adder_coverage bf16_adder_cov;
- 
+   bf16adder_coverage bf16_adder_cov = new();
+  
    // Design under test instance
-   bf16adder dut (.a(input1),
-		  .b(input2),
-		  .sum(out));
+   bfloat16adder dut (.a(input1), .b(input2), .sum(out));
 
    // Testbench
    initial begin
+      $dumpfile("dump.vcd"); 
+      $dumpvars;
+     
       // Initialize input signals
       input1 = 16'h0000;
       input2 = 16'h0000;
 
-      // Coverage reset
-      bf16_adder_cov.reset();
-
+      bf16_adder_cov.sample();
+     
       // Generation of tests
       repeat (NUM_TESTS) begin
-	 input1 = $random;
-	 input2 = $random;
-	 $display("Test: input1 = %p, input2 = %p", input1, input2);
-	 #10;
+        $display("[%0t] Test: input1 = %h, input2 = %h, output = %h", $time+1/10, input1, input2, out);
+	 	input1 = $random;
+	 	input2 = $random;
+        #10;
       end
-
-      $finish;
    end // initial begin
 
-   final begin
-      bf16_adder_cov.write();
+   initial begin
+     #1000 $display ("Coverage = %0.2f %%", bf16_adder_cov.get_coverage());
+     $finish;
    end
 
-endprogram // test_bfloat
+endmodule // test_bfloat
